965dbd7343d654a784f8e43dbc17039a6ad339e1
Dariia Hryshchukova <dariagrisukova@gmail.com>
2025-08-04 19:15:53 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,39 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const div = document.createElement('div');
+
+  div.classList.add('notification');
+
+  if (type === 'success') {
+    div.classList.add('success');
+  }
+
+  if (type === 'error') {
+    div.classList.add('error');
+  }
+
+  if (type === 'warning') {
+    div.classList.add('warning');
+  }
+
+  const h = document.createElement('h2');
+
+  h.classList.add('title');
+
+  const p = document.createElement('p');
+
+  div.style.top = posTop + 'px';
+  div.style.right = posRight + 'px';
+
+  h.textContent = title;
+  p.textContent = description;
+  div.append(h, p);
+  document.body.appendChild(div);
+
+  setTimeout(() => {
+    div.style.display = 'none';
+  }, 2000);
 };
 
 pushNotification(
